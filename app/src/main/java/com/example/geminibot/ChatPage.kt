c51545964c2d382ff2a262787ed533c012eff0a5
tejasM17 <tejasm9380@gmail.com>
2026-04-16 17:37:41 +0530

    removed some imports that is not used in the mainactivity

--- app/src/main/java/com/example/geminibot/ChatPage.kt
+++ app/src/main/java/com/example/geminibot/ChatPage.kt
@@ -4,16 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun ChatPage(modifier: Modifier = Modifier) {
-
-}
-
-@Composable
-fun MessageBubble(modifier: Modifier = Modifier) {
-
-}
-
-@Composable
-fun MessageInput(modifier: Modifier = Modifier) {
-
+fun ChatScreen(modifier: Modifier = Modifier) {
+    
 }
--- app/src/main/java/com/example/geminibot/ChatViewmodel.kt
+++ app/src/main/java/com/example/geminibot/ChatViewmodel.kt
@@ -3,9 +3,7 @@ package com.example.geminibot
 import androidx.lifecycle.ViewModel
 
 class ChatViewmodel: ViewModel() {
-
     fun sendMessage() {
 
     }
-
 }
